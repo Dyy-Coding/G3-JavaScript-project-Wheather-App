363f5aab866e4a9c35428b14788dc2f158191139
Sreynich Chhoeurn <sreynich.chhoeurn@student.passerellesnumeriques.org>
2025-01-21 12:27:31 +0700

    location_page#16

---
+++ source/js/page-js/location.js
@@ -0,0 +1,11 @@
+document.querySelector('.zoom-in').addEventListener('click', () => {
+    alert('Zoom-in functionality coming soon!');
+});
+
+document.querySelector('.zoom-out').addEventListener('click', () => {
+    alert('Zoom-out functionality coming soon!');
+});
+
+document.querySelector('.navigate').addEventListener('click', () => {
+    alert('Navigation functionality coming soon!');
+});
